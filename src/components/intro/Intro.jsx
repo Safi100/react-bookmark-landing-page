8c05ba5fd8d5333f6ddf5869f40ce141cc4df14b
safi100 <safinafi12@gmail.com>
2022-07-27 22:58:23 +0300

    finish intro

---
+++ src/components/intro/Intro.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import "./intro.css"
+import Illustration_hero from "../../Assets/illustration-hero.svg"
+const Intro = () => {
+  return (
+    <div className='intro'>
+      <div className="wrapper">
+        <div className="left">
+          <h2>A Simple Bookmark Manager</h2>
+          <p>A clean and simple interface to organize your favourite<br/>websites. Open a new 
+          browser tab and see your sites<br/>load instantly. Try it for free.</p>
+          <div className="buttons">
+            <button className='btn chrome_btn'>Get it on Chrome</button>
+            <button className='btn firefox_btn'>Get it on Firefox</button>
+          </div>
+        </div>
+        <div className="right">
+          <div className="draw"></div>
+          <img src={Illustration_hero} alt="Illustration_hero" />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Intro
