4ccb86af1a87fc502f08d6bb16e5ac2814c7d9ae
Safi100 <safinafi12@gmail.com>
2022-07-29 15:59:14 +0300

    fix js click

--- src/components/questions/Question.jsx
+++ src/components/questions/Question.jsx
@@ -3,8 +3,8 @@ import React, { useState } from 'react'
 const Question = ({question, answer}) => {
     const [open, setOpen] = useState(false)
   return (
-    <div className="question_answer_container" onClick={() => setOpen(!open)}>
-        <div className="question">
+    <div className="question_answer_container">
+        <div className="question" onClick={() => setOpen(!open)}>
             <span className='question_title'>{question}</span>
             <div className={`arrow ${(open === true) ? "arrow_in_open" : ""}`}><svg xmlns="http://www.w3.org/2000/svg" width="18" height="12"><path className={`${(open === true) ? "arrow_color_in_open" : ""}`} fill="none" stroke="#5267DF" stroke-width="3" d="M1 1l8 8 8-8"/></svg></div>
         </div>
