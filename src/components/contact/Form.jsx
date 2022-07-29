a6f7d70d651b355293b8054163a717d05686ad5c
Safi100 <safinafi12@gmail.com>
2022-07-29 22:22:50 +0300

    finish form

--- src/components/contact/Form.jsx
+++ src/components/contact/Form.jsx
@@ -4,8 +4,7 @@ const Form = () => {
   return (
     <form>
     <div className="left_in_form">
-      <div className="input"><input type="text"/><div className="error_icon"></div></div>
-      <div className='error_div'><span className='error_message'>Whoops, Make sure it's an email</span></div>
+      <input type="email" placeholder='example@gmail.com' required/>
     </div>
     <div className='right_in_form'><button className='btn red_btn'>Contact Us</button></div>
   </form>
